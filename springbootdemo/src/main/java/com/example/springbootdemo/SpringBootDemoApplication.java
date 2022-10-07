62e25e2ab213c5d2cb6d5ca4b14370fc2bc15a30
Pratiksha Kute <pratikute1603@gmail.com>
2022-10-07 13:02:10 +0530

    initial version

--- springbootdemo/src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ springbootdemo/src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
@@ -22,25 +23,32 @@ public class SpringBootDemoApplication {
 //		System.out.println(user1);
 
 //creating user
-		User user= new User();
-	    user.setName("Pratiksha");
-        user.setCity("Pune");
-
-		User user1=new User();
-		user1.setName("Sanket");
-		user1.setCity("A.nagar");
+//		User user= new User();
+//	    user.setName("Pratiksha");
+//        user.setCity("Pune");
+//
+//		User user1=new User();
+//		user1.setName("Sanket");
+//		user1.setCity("A.nagar");
 
 //saving single user
-		User user2=uSerRepo.save(user1);
-		System.out.println("User save"+user2);
-		System.out.println("done");
+//		User user2=uSerRepo.save(user1);
+//		System.out.println("User save"+user2);
+//		System.out.println("done");
 
 //saving multiple user
-		List<User>users=List.of(user,user1);
-		Iterable<User> result=USerRepo.saveAll(users);
-		result.forEach(User->{
-			System.out.println(result);
-		});
+//		List<User>users=List.of(user,user1);
+//		Iterable<User> result=USerRepo.saveAll(users);
+//		result.forEach(User->{
+//			System.out.println(result);
+//		});
+
+// update the user of  id 2
+         Optional<User> optional =uSerRepo.findById(2);
+		 User user= optional.get();
+		 user.setCity("Ahemadnagar");
+		 User result=uSerRepo.save(user);
+		System.out.println(result);
 
 
 	}
